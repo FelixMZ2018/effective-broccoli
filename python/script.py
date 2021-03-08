b7a91cbf906b25028db37d1c6823ed791ce3db87
Felix <FelixinLondon@googlemail.com>
2021-03-08 23:36:48 +0100

    cheking for env variables

--- python/script.py
+++ python/script.py
@@ -1,1 +1,10 @@
+import os
+
 print("Hello World")
+
+temperature = os.environ['TEMPERATURE']
+light_sensor = os.environ['LIGHT']
+No_of_Soil = os.environ['SOIL'] 
+print(f'Using {temperature} as temperature sensor')
+print(f'Using {light_sensor} as light sensor')
+print(f'confiuration for {No_of_Soil} soil moisture sensors')
